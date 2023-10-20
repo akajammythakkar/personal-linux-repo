9bcb7348a5661d9a1e69a947169812bd51acf373
Jay Thakkar <76893091+akajammythakkar@users.noreply.github.com>
2023-10-20 11:41:54 +0530

    Add files via upload

---
+++ packages.sh
@@ -0,0 +1,4 @@
+sudo pacman -Syu -y
+sudo pacman -Sy firefox vim gnome-themes-extra gnome-tweaks gparted neofetch screenfetch git ntfs-3g libreoffice grub-customizer
+#git clone https://aur.archlinux.org/whatsapp-for-linux.git
+
